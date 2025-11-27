9614130ecd5a5f14bdde451258fe100c61ee50f9
YagoRosa <30865549+YagoRosa@users.noreply.github.com>
2025-11-27 12:50:02 -0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const sendMessagesAbsenteismo = require('./functions/sendMessagesAbsenteismo');
-const sendMessagesInterncao = require('./functions/sendMessagesInterncao');
-const sendMessagesConsultaColono = require('./functions/sendMessagesConsultaColono');
-const sendMessagesExameColono = require('./functions/sendMessagesExameColono');
-const sendMessagesExameEDA = require('./functions/sendMessagesExameEDA');
-
-module.exports = { sendMessagesAbsenteismo, sendMessagesInterncao, sendMessagesConsultaColono, sendMessagesExameColono, sendMessagesExameEDA};
